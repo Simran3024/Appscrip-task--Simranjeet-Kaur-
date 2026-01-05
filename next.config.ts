4b879d5861788db4995911ad08739c2fcc2bac5c
Simranjeet Kaur <108970117+Simran3024@users.noreply.github.com>
2026-01-05 18:04:57 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
   devIndicators: {
     buildActivity: false,
+  },
+
+  images: {
+    domains: ["fakestoreapi.com"],
   },
 };
 
